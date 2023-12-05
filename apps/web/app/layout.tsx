67e646bc7025d9790d9f05e56725d5fe240fb843
Jonas Scholz <Jonas.Scholz@bbscholz.de>
2023-12-05 09:59:26 +0100

    feat: add plausible

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,5 +1,6 @@
 import "@ui/styles/globals.css";
 import { Poppins } from "next/font/google";
+import PlausibleProvider from "next-plausible";
 
 const inter = Poppins({
   weight: ["400", "500", "600", "700"],
@@ -12,9 +13,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={inter.className}>
-      <body className={`bg-gray-100 antialiased ${inter.className} font-sans`}>
-        {children}
-      </body>
+      <PlausibleProvider domain="devto-wrapped.sliplane.app">
+        <body
+          className={`bg-gray-100 antialiased ${inter.className} font-sans`}
+        >
+          {children}
+        </body>
+      </PlausibleProvider>
     </html>
   );
 }
