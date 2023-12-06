96ed3067f1bb81cb653a9c187f4d171b3f9ea8dd
Jonas Scholz <Jonas.Scholz@bbscholz.de>
2023-12-06 18:13:12 +0100

    feat: align

--- apps/web/components/cards/favorite-tag.tsx
+++ apps/web/components/cards/favorite-tag.tsx
@@ -6,41 +6,43 @@ export default function FavoriteTag({
   bestPerformingTag: string;
 }) {
   return (
-    <div className="border border-gray-300 rounded-xl shadow-md w-full p-4 bg-white">
-      {favoriteTag === bestPerformingTag ? (
-        <>
-          Your favorite and best performing tag was
-          <a
-            className="underline underline-offset-2 ml-1"
-            rel="noopener noreferrer"
-            target="_blank"
-            href={`https://dev.to/t/${favoriteTag}`}
-          >
-            #{favoriteTag}
-          </a>
-          !
-        </>
-      ) : (
-        <>
-          Although your favorite tag was
-          <a
-            className="underline underline-offset-2 ml-1"
-            rel="noopener noreferrer"
-            target="_blank"
-            href={`https://dev.to/t/${favoriteTag}`}
-          >
-            #{favoriteTag}
-          </a>
-          , your best performing tag was
-          <a
-            className="underline underline-offset-2 ml-1"
-            href={`https://dev.to/t/${bestPerformingTag}`}
-          >
-            #{bestPerformingTag}
-          </a>
-          <span className="ml-1 text-base">👀</span>
-        </>
-      )}
+    <div className="border flex flex-col justify-center border-gray-300 rounded-xl shadow-md w-full p-4 bg-white">
+      <div>
+        {favoriteTag === bestPerformingTag ? (
+          <>
+            Your favorite and best performing tag was
+            <a
+              className="underline underline-offset-2 ml-1"
+              rel="noopener noreferrer"
+              target="_blank"
+              href={`https://dev.to/t/${favoriteTag}`}
+            >
+              #{favoriteTag}
+            </a>
+            !
+          </>
+        ) : (
+          <>
+            Although your favorite tag was
+            <a
+              className="underline underline-offset-2 ml-1"
+              rel="noopener noreferrer"
+              target="_blank"
+              href={`https://dev.to/t/${favoriteTag}`}
+            >
+              #{favoriteTag}
+            </a>
+            , your best performing tag was
+            <a
+              className="underline underline-offset-2 ml-1"
+              href={`https://dev.to/t/${bestPerformingTag}`}
+            >
+              #{bestPerformingTag}
+            </a>
+            <span className="ml-1 text-base">👀</span>
+          </>
+        )}
+      </div>
     </div>
   );
 }
