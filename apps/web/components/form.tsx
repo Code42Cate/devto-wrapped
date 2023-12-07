d248c95f765e3b9bb9b6374bf79f9d16b82285d7
Jonas Scholz <Jonas.Scholz@bbscholz.de>
2023-12-07 16:14:17 +0100

    feat: add loading statE

--- apps/web/components/form.tsx
+++ apps/web/components/form.tsx
@@ -1,13 +1,23 @@
 "use client";
 import { Button } from "@ui/components/button";
 import { Input } from "@ui/components/input";
-import { useState } from "react";
-import { useRouter } from "next/navigation";
+import { Loader2 } from "lucide-react";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function UsernameForm() {
   const [username, setUsername] = useState("");
 
   const { push } = useRouter();
+
+  const searchParams = useSearchParams();
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    if (searchParams.get("username") === username) {
+      setIsLoading(false);
+    }
+  }, [setIsLoading, searchParams, username]);
 
   return (
     <div className="flex flex-row gap-2">
@@ -20,12 +30,13 @@ export default function UsernameForm() {
       />
 
       <Button
+        disabled={isLoading}
         onClick={() => {
-          console.log(username);
+          setIsLoading(true);
           push(`/?username=${username}`);
         }}
       >
-        👀
+        {isLoading ? <Loader2 className="h-4 w-4 animate-spin" /> : "👀"}
       </Button>
     </div>
   );
